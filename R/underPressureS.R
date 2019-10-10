83cf3752df9274b7882d5d2d87d6775480c8ed08
Kurt Van Balen <kurt.vanbalen@student.ucll.be>
2019-10-10 11:00:56 +0200

    added second sensor

--- R/underPressureS.R
+++ R/underPressureS.R
@@ -17,18 +17,27 @@ pressureWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "20
   )
 
   # You can fetch all results
-  querry <- paste(paste("SELECT Timestamp, Pressure from PressureData where SensorID =", sensorID, sep = " "),"AND Timestamp BETWEEN '" ,sep = " ")
+  #query voor 1 sensor tegelijk
+  #querry <- paste(paste("SELECT Timestamp, Pressure from PressureData where SensorID =", sensorID, sep = " "),"AND Timestamp BETWEEN '" ,sep = " ")
+  #querry <- paste0(paste0(paste0(paste0(querry, dateFrom), "%' AND '"), dateTo),"%';")
+
+  #querry voor 2 sensors tegelijk
+  querry <- paste0(paste0(paste0("Select * FROM (select *
+                                 from (
+                                 select Timestamp, GROUP_CONCAT(case when SensorID=", sensorID)," then Humidity end) as Sensor1, GROUP_CONCAT(case when SensorID=",sensorID2)," then Humidity end) as Sensor2
+                   from HumidityData
+                   group by Timestamp
+                                 ) as sub
+                   where Sensor1 is not NULL
+                   or Sensor2 is not NULL) as t1 WHERE Timestamp BETWEEN '")
   querry <- paste0(paste0(paste0(paste0(querry, dateFrom), "%' AND '"), dateTo),"%';")
-  #querry1 <- paste("SELECT Timestamp, Pressure FROM PressureData WHERE SensorID =", sensorID, sep = " ")
-  #uerry2 <- paste("SELECT Timestamp, Pressure FROM PressureData WHERE SensorID =", sensorID2, sep = " ")
-  #querry3 <- paste0(paste0(paste0(paste0(paste0(paste0("SELECT * FROM (", querry1),") AS tabel1 LEFT JOIN ("),querry2),") AS tabel2 ON tabel1.Timestamp = tabel2.Timestamp UNION SELECT * FROM (",querry1),") AS t1 RIGHT JOIN (", querry2), ") AS t2 ON t1.Timestamp = t2.Timestamp;")
   res <- dbSendQuery(con, querry)
   res2 <-dbFetch(res)
   #res2
   #data frame aanpassen/ xts objects nodig voor time
   res2 <- xts::xts(res2[,-1], order.by = res2$Timestamp)
   # plot maken
-  PressureG <- dygraph(res2) %>% dyRangeSelector()  %>% dySeries("V1",color = "green")
+  PressureG <- dygraph(res2) %>% dyRangeSelector()  %>% dySeries("Sensor1",color = "green") %>% dyOptions(connectSeparatedPoints = TRUE)
   htmlwidgets::saveWidget(widgetframe::frameableWidget(PressureG),'dygraph_pressure_self.html', selfcontained = TRUE)
   # Clear the result
   dbClearResult(res)
@@ -37,7 +46,7 @@ pressureWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "20
   dbDisconnect(con)
 }
 
-tempWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-09-30")
+tempWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-09-30",sensorID2 = 1207)
 {
   require(RMariaDB)
   require(DBI)
@@ -56,7 +65,14 @@ tempWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-0
   )
 
   # You can fetch all results:
-  querry <- paste(paste("SELECT Timestamp, Temperature from TempData where SensorID =", sensorID, sep = " "),"AND Timestamp BETWEEN '" ,sep = " ")
+  querry <- paste0(paste0(paste0("Select * FROM (select *
+                                 from (
+                                 select Timestamp, GROUP_CONCAT(case when SensorID=", sensorID)," then Humidity end) as Sensor1, GROUP_CONCAT(case when SensorID=",sensorID2)," then Humidity end) as Sensor2
+                   from HumidityData
+                   group by Timestamp
+                                 ) as sub
+                   where Sensor1 is not NULL
+                   or Sensor2 is not NULL) as t1 WHERE Timestamp BETWEEN '")
   querry <- paste0(paste0(paste0(paste0(querry, dateFrom), "%' AND '"), dateTo),"%';")
   res <- dbSendQuery(con, querry)
   res2 <-dbFetch(res)
@@ -65,7 +81,7 @@ tempWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-0
   #data frame aanpassen/ xts objects nodig voor time
   res2 <- xts::xts(res2[,-1], order.by = res2$Timestamp)
   # plot maken
-  TempG <- dygraph(res2) %>% dyRangeSelector()  %>% dySeries("V1",color = "red")
+  TempG <- dygraph(res2) %>% dyRangeSelector()  %>% dySeries("Sensor1",color = "red")
   htmlwidgets::saveWidget(widgetframe::frameableWidget(TempG),'dygraph_temp_self.html', selfcontained = TRUE)
   # Clear the result
   dbClearResult(res)
@@ -74,7 +90,7 @@ tempWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-0
   dbDisconnect(con)
 }
 
-humidWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-09-30")
+humidWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-09-30",sensorID2 = 1207)
 {
   require(RMariaDB)
   require(DBI)
@@ -93,7 +109,14 @@ humidWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-
   )
 
   # You can fetch all results:
-  querry <- paste(paste("SELECT Timestamp, Humidity from HumidityData where SensorID =", sensorID, sep = " "),"AND Timestamp BETWEEN '" ,sep = " ")
+  querry <- paste0(paste0(paste0("Select * FROM (select *
+                                 from (
+                                 select Timestamp, GROUP_CONCAT(case when SensorID=", sensorID)," then Humidity end) as Sensor1, GROUP_CONCAT(case when SensorID=",sensorID2)," then Humidity end) as Sensor2
+                   from HumidityData
+                   group by Timestamp
+                                 ) as sub
+                   where Sensor1 is not NULL
+                   or Sensor2 is not NULL) as t1 WHERE Timestamp BETWEEN '")
   querry <- paste0(paste0(paste0(paste0(querry, dateFrom), "%' AND '"), dateTo),"%';")
   res <- dbSendQuery(con, querry)
   res2 <-dbFetch(res)
@@ -102,7 +125,7 @@ humidWidget <- function(sensorID = 929, dateFrom = "2019-09-01", dateTo = "2019-
   #data frame aanpassen/ xts objects nodig voor time
   res2 <- xts::xts(res2[,-1], order.by = res2$Timestamp)
   # plot maken
-  HumidG <- dygraph(res2) %>% dyRangeSelector()  %>% dySeries("V1",color = "blue")
+  HumidG <- dygraph(res2) %>% dyRangeSelector()  %>% dySeries("Sensor1",color = "blue")
   htmlwidgets::saveWidget(widgetframe::frameableWidget(HumidG),'dygraph_humid_self.html', selfcontained = TRUE)
   # Clear the result
   dbClearResult(res)
